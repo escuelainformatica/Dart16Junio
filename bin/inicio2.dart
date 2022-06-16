6010f2327152e32f18f9e4d5ac6cd6e2b7cc2176
jorgecc <jcastro@eftec.cl>
2022-06-16 10:09:09 -0400

    16/06/2022

--- bin/inicio2.dart
+++ bin/inicio2.dart
@@ -1,6 +1,17 @@
 
+import 'modelo/cliente.dart';
+import 'modelo/libro.dart';
+
 main(List<String> arguments) {
 
   print("hola mundo");
-  
+
+  Cliente cli=Cliente("2222-2", "john", "doe",555);
+  Cliente cli2=Cliente.adicional("2222-2", "john", "doe");
+  Cliente cli3=Cliente.convencional("33333-3", "john", "doe");
+  Cliente cli4=Cliente.sinDeuda("2222-2", "john", "doe");
+
+  Libro libro=Libro("Harry Potter", null,300,null);
+  Libro libro2=Libro.nombre(titulo: "Harry Potter",numPagina: 300);
+
 }
---
+++ bin/modelo/libro.dart
@@ -0,0 +1,14 @@
+class Libro {
+  String titulo;
+  String? autor;
+  int numPagina;
+  String? compania;
+
+  Libro(this.titulo,this.autor,this.numPagina,this.compania);
+  Libro.nombre({
+    required this.titulo,
+    this.autor,
+    this.numPagina=300,
+    this.compania}
+      );
+}
